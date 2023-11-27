c7354edb2bbf5cbc1b8ffd0d5f2335dcfd98e62b
Zaar97 <128958228+Zaar97@users.noreply.github.com>
2023-11-27 13:34:53 +0700

    backend payload done

---
+++ blogback/src/collections/Blogs.ts
@@ -0,0 +1,103 @@
+import { CollectionConfig } from 'payload/types'
+import payload from 'payload'
+import Content from '../blocks/Content';
+
+const Blogs: CollectionConfig = {
+  slug: 'Blogs',
+  access : {
+    read : () => true,
+    update : () => true,
+    delete : () => true,
+    create : () => true
+  },
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'postMeta',
+      type: 'group',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+          required: true,
+          minLength: 20,
+          maxLength: 100,
+        },
+        {
+          name: 'description',
+          type: 'textarea',
+          required: true,
+          minLength: 40,
+          maxLength: 160,
+        },
+      ],
+    },
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          label: 'Post Media',
+          fields: [
+            {
+              name: 'postImage',
+              type: 'upload',
+              relationTo: 'media',
+              required: true,
+            },
+          ],
+        },
+        {
+          label: 'Post Layout',
+          fields: [
+            {
+              name: 'layout',
+              type: 'blocks',
+              blocks: [
+                Content
+              ],
+            },
+          ]
+        }
+      ]
+    },
+  ],
+
+  hooks: {
+    afterOperation: [
+      async (args) => {
+        if (args.operation == "create") {
+          payload.create({
+            collection: "logs",
+            data: {
+              collect: "Blogs",
+              action: "Create",
+              timestamp: new Date(),
+            },
+          });
+        } else if (args.operation == "update") {
+          payload.create({
+            collection: "logs",
+            data: {
+              collect: "Blogs",
+              action: "Update",
+              timestamp: new Date(),
+            }
+          });
+        } else if (args.operation == "delete") {
+          payload.create({
+            collection: "logs",
+            data: {
+              collect: "Blogs",
+              action: "Delete",
+              timestamp: new Date(),
+            }
+          });
+        }
+      },
+    ],
+  },
+}
+
+export default Blogs
---
+++ blogback/src/payload.config.ts
@@ -0,0 +1,38 @@
+import path from 'path'
+
+import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import { buildConfig } from 'payload/config'
+
+import Users from './collections/Users'
+import Categories from './collections/Categories'
+import Blogs from './collections/Blogs'
+import Logs from './collections/Logs'
+import Media from './collections/Media'
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+  },
+  editor: slateEditor({}),
+  collections: [
+    Users, 
+    Categories, 
+    Blogs, 
+    Logs, 
+    Media,
+  ],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  plugins: [payloadCloud()],
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
+  }),
+})

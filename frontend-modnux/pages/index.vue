f49336aa6e4b70e06e2e1e62776ec21ff5d58679
sgra <rerodeja@gmail.com>
2023-11-29 00:38:51 +0700

    update: nuxt
    nuxt update

---
+++ frontend-modnux/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Index</h2>
+        <p>lorem ipsum</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
